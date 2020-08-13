fcdf5e632736f93335c6d9c2bd67c3eba8072a12
Himanshu <yadavhimanshu63@gmail.com>
2020-08-13 18:19:18 +0530

    First Name

--- userRegisteration.sh
+++ userRegisteration.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "welcome to user registeration program"
+
+function isFirstName()
+{
+   read -p "enter first name :" firstName
+   pattern='(^[[:upper:]][[:alpha:]]{2,14}$)'
+   if [[ $firstName =~ $pattern  ]]
+   then
+         echo "First name : $firstName"
+   else
+         echo "enter valid first name"
+   fi
+}
+isFirstName
